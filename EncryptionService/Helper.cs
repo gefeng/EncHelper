81c6dbf966559466a52d727e7be3b4bdc843868b
ge.feng <gefengsteve@gmail.com>
2017-02-20 19:41:19 +0000

    Revert "Revert "Init commit""
    This reverts commit 964023f1c50ba2eaeb244fa06c898a507961499c.

---
+++ EncryptionService/Helper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncHelper
+{
+    internal static class Helper
+    {
+        public static int HexCharToInt(char hexChar)
+        {
+            if (hexChar >= '0' && hexChar <= '9')
+            {
+                return (int)(hexChar - '0');
+            }
+
+            char upper = char.ToUpper(hexChar);
+
+            if(hexChar >= 'A' && hexChar <= 'F')
+            {
+                return 10 + (int)(hexChar - 'A');
+            }
+
+            throw new ArgumentOutOfRangeException("hexChar");
+        }
+    }
+}
---
+++ EncryptionService/IEncryptionService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+
+namespace EncHelper.EncryptionService
+{
+    [ServiceContract(Namespace = NSNames.NSENCHELPER1702, Name = "EncHelper")]
+    public interface IEncryptionService
+    {
+        [OperationContract]
+        string Xor(string blockA, string blockB);
+
+        [OperationContract]
+        string CalPinBlock(string pan, string pin, PinBlockFormat format);
+    }
+
+    [DataContract(Namespace = NSNames.NSENCHELPER1702, Name = "PinBlockFormat")]
+    public enum PinBlockFormat
+    {
+        [EnumMember]
+        ISO0,
+        [EnumMember]
+        VISA3,
+    }
+}
