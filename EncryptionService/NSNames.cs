cc5db1dfe9b04d40d072aa3f80fe1d543dbcae38
ge.feng <gefengsteve@gmail.com>
2017-02-20 19:27:05 +0000

    Init commit

---
+++ EncryptionService/NSNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncHelper
+{
+    internal static class NSNames
+    {
+        public const string NSENCHELPER = "http://gefeng.com/EncHelper";
+        public const string NSENCHELPER1702 = NSENCHELPER + "/2017/02";
+    }
+}
